f96396293d59f3ebc2538b77c7646db0d0e10b7b
yiqiwang <yiqiwang@tencent.com>
2017-06-06 21:10:18 +0800

    删除自带的storyboard，增加navigationController和rootviewController

--- LearnAnimations/AppDelegate.h
+++ LearnAnimations/AppDelegate.h
@@ -6,12 +6,16 @@
 //  Copyright © 2017年 melody5417. All rights reserved.
 //
 
+/**
+ * Learn iOS animations 
+ * Example Project is from https://github.com/YouXianMing
+ */
+
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
 
 @end
 
---
+++ LearnAnimations/RootController/AnimationsListController.m
@@ -0,0 +1,39 @@
+//
+//  AnimationsListController.m
+//  LearnAnimations
+//
+//  Created by yiqiwang(王一棋) on 2017/6/6.
+//  Copyright © 2017年 melody5417. All rights reserved.
+//
+
+#import "AnimationsListController.h"
+
+@interface AnimationsListController ()
+
+@end
+
+@implementation AnimationsListController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+    self.view.layer.backgroundColor = [UIColor redColor].CGColor;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
